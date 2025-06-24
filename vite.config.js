7e1dfff09bb4e093e5076beb019c913a4288f718
Louis-HN <louishen@hotmail.fr>
2025-06-23 23:22:35 -0400

    Fix: move index.html to root for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 
-export default {
-  root: '.', // base = dossier contenant index.html
-  build: {
-    outDir: 'dist',
-  },
-};
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // la racine contient le index.html
+  publicDir: 'public', // optionnel si tu veux y garder des assets
+});
