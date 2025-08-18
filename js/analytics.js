bfbc9d39c88cf72b62aad204175389c04b11fdb5
Louis-HN <louishen@hotmail.fr>
2025-08-18 14:06:36 -0400

    fix(all): analytics script

--- js/analytics.js
+++ js/analytics.js
@@ -5,25 +5,12 @@ window.va = window.va || function () {
   (window.vaq = window.vaq || []).push(arguments); 
 };
 
-// Speed Insights
-window.si = window.si || function () { 
-  (window.siq = window.siq || []).push(arguments); 
-};
-
 // Injection Vercel Analytics
 function injectAnalytics() {
   const analyticsScript = document.createElement('script');
   analyticsScript.src = 'https://cdn.vercel-insights.com/v1/script.js';
   analyticsScript.defer = true;
   document.head.appendChild(analyticsScript);
-}
-
-// Injection Speed Insights
-function injectSpeedInsights() {
-  const speedScript = document.createElement('script');
-  speedScript.src = 'https://cdn.vercel-insights.com/v1/speed-insights/script.js';
-  speedScript.defer = true;
-  document.head.appendChild(speedScript);
 }
 
 // Initialisation
